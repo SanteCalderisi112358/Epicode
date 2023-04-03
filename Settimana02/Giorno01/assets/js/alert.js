4f6571993adb3c53f550dfa1947c936013d5ed22
SanteCalderisi112358 <santecalderisi@gmail.com>
2023-04-03 17:11:31 +0200

    esercizio

---
+++ Settimana02/Giorno01/assets/js/alert.js
@@ -0,0 +1,7 @@
+
+
+var primoAlert = "Sei sicuro?"
+var SecondoAlert = "Ma davvero sicuro?"
+
+window.alert(primoAlert);
+window.alert(SecondoAlert);
---
+++ Settimana02/Giorno01/assets/js/altreScritte.js
@@ -0,0 +1,23 @@
+
+
+
+var titoloH1 = "<h1>Iniziamo la nostra pratica</h1>";
+
+document.getElementById('titolo').innerHTML = titoloH1;
+
+var primoTesto = "output scritto da Javascript";
+
+
+document.write(primoTesto) = primoTesto;
+
+var SecondoTesto = "pantera scritto da Javascript";
+document.write(Secondo_Testo) = Secondo_Testo;
+
+
+var criceto = "criceto";
+var giraffa = "giraffa";
+var leone = "leone";
+
+console.log(criceto);
+console.log(giraffa);
+console.log(leone);
