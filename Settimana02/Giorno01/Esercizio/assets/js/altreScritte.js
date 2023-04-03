593dfc0cdfff3bff718dde50d213d6672b38bbec
SanteCalderisi112358 <santecalderisi@gmail.com>
2023-04-04 00:12:08 +0200

    k

--- Settimana02/Giorno01/Esercizio/assets/js/altreScritte.js
+++ Settimana02/Giorno01/Esercizio/assets/js/altreScritte.js
@@ -6,18 +6,23 @@ var titoloH1 = "<h1>Iniziamo la nostra pratica</h1>";
 document.getElementById('titolo').innerHTML = titoloH1;
 
 var primoTesto = "output scritto da Javascript";
+document.getElementById('primoTesto').innerHTML = primoTesto;
+var Secondo_Testo = "pantera scritto da Javascript";
+document.getElementById('Secondo_Testo').innerHTML = Secondo_Testo;
+
+//document.write('primoTesto');
 
 
-document.write(primoTesto) = primoTesto;
-
-var SecondoTesto = "pantera scritto da Javascript";
-document.write(Secondo_Testo) = Secondo_Testo;
 
 
-var criceto = "criceto";
-var giraffa = "giraffa";
-var leone = "leone";
 
 console.log('criceto');
-console.log(giraffa);
-console.log(leone);
+
+
+console.log('giraffa');
+
+
+console.log('leone');
+
+
+
