a780f54fcc4caa3330fbe00c1613e67228ae2005
SanteCalderisi112358 <santecalderisi@gmail.com>
2023-04-05 23:52:01 +0200

    s

---
+++ Settimana02/Giorno03/Lezione/assets/js/form.js
@@ -0,0 +1,43 @@
+const nomeUtente = document.getElementById('nome');
+const cognomeUtente = document.getElementById('cognome');
+const annoUtente = document.getElementById('anno');
+sendBtn = document.getElementById('invia');
+
+sendBtn.addEventListener('click', callBack)  
+
+var mioNome;
+var mioCognome;
+var mioAnno;
+
+function callBack(){
+if (controlla()) {
+		conferma();
+	}
+}
+
+function controlla() {
+	 mioNome = nomeUtente.value;
+	 mioCognome = cognomeUtente.value;
+     mioAnno = annoUtente.value;
+     console.log(mioAnno);
+	if (mioNome == '') {
+		alert('Inserire il nome utente')
+		return false;
+	} else if (mioCognome == '') {
+		alert('Inserire il cognome utente')
+		return false;
+	}  if(annoUtente == ''){
+        alert('Inserire anno di nascita')
+		return true;
+	}
+}
+
+function conferma() {
+    
+    var eta = 2023-mioAnno;
+    console.log(eta);
+	document.getElementById('conferma').innerHTML = 'Form inviato correttamente; dati inseriti:<br>Nome: '+' '+mioNome+ '- Cognome: '+mioCognome+'.La tua età è: '+eta;
+    nomeUtente.value = '';
+    cognomeUtente.value = '';
+    annoUtente.value ='';
+}
