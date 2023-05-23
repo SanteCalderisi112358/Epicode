05ff4c972c9e66b0165ebc04b7ba5aa368ca5dda
SanteCalderisi112358 <santecalderisi@gmail.com>
2023-05-23 16:04:35 +0200

    s

--- Settimana09/Giorno02/Esercitazione/assets/js/esercizio.js
+++ Settimana09/Giorno02/Esercitazione/assets/js/esercizio.js
@@ -18,16 +18,16 @@ var SonAccount = /** @class */ (function () {
         var _this = this;
         this.balanceInit = 0;
         this.oneDeposit = function (primoVersamento) {
-            return "Saldo Iniziale = ".concat(_this.balanceInit, "\u20AC\nPrimo Versamento = ").concat(primoVersamento, "\u20AC");
+            return "Saldo Iniziale ".concat(_this.balanceInit, "\u20AC\nPrimo Versamento ").concat(primoVersamento, "\u20AC");
         };
         this.oneWhiteDraw = function (primoVersamento, primoPrelievo) {
-            return "".concat(_this.oneDeposit(primoVersamento), "\nPrimo Prelievo = ").concat(primoPrelievo, "\u20AC");
+            return "".concat(_this.oneDeposit(primoVersamento), "\nPrimo Prelievo ").concat(primoPrelievo, "\u20AC");
         };
         this.twoDeposit = function (primoVersamento, primoPrelievo, secondoVersamento) {
-            return "".concat(_this.oneWhiteDraw(primoVersamento, primoPrelievo), "\nSecondo Versamento = ").concat(secondoVersamento, "\u20AC");
+            return "".concat(_this.oneWhiteDraw(primoVersamento, primoPrelievo), "\nSecondo Versamento ").concat(secondoVersamento, "\u20AC");
         };
         this.twoWithDraw = function (primoVersamento, primoPrelievo, secondoVersamento, secondoPrelievo) {
-            return "".concat(_this.twoDeposit(primoVersamento, primoPrelievo, secondoVersamento), "\nSecondo Prelievo = ").concat(secondoPrelievo, "\u20AC\nIl saldo finale \u00E8 = ").concat(_this.balanceInit + primoVersamento + secondoVersamento - primoPrelievo - secondoPrelievo, "\u20AC");
+            return "".concat(_this.twoDeposit(primoVersamento, primoPrelievo, secondoVersamento), "\nSecondo Prelievo ").concat(secondoPrelievo, "\u20AC\nIl saldo finale \u00E8 = ").concat(_this.balanceInit + primoVersamento + secondoVersamento - primoPrelievo - secondoPrelievo, "\u20AC");
         };
         this.balanceInit = _balanceInit;
     }
@@ -38,18 +38,6 @@ var MotherAccount = /** @class */ (function (_super) {
     function MotherAccount(_balanceInit) {
         var _this = _super.call(this, _balanceInit) || this;
         _this.balanceInit = 0;
-        _this.oneDeposit = function (primoVersamento) {
-            return "Saldo Iniziale = ".concat(_this.balanceInit, "\u20AC\nPrimo Versamento ").concat(primoVersamento, "\u20AC");
-        };
-        _this.oneWhiteDraw = function (primoVersamento, primoPrelievo) {
-            return "".concat(_this.oneDeposit(primoVersamento), "\nPrimo Prelievo ").concat(primoPrelievo, "\u20AC");
-        };
-        _this.twoDeposit = function (primoVersamento, primoPrelievo, secondoVersamento) {
-            return "".concat(_this.oneWhiteDraw(primoVersamento, primoPrelievo), "\nSecondo Versamento ").concat(secondoVersamento, "\u20AC");
-        };
-        _this.twoWithDraw = function (primoVersamento, primoPrelievo, secondoVersamento, secondoPrelievo) {
-            return "".concat(_this.twoDeposit(primoVersamento, primoPrelievo, secondoVersamento), "\nSecondo Prelievo ").concat(secondoPrelievo, "\u20AC\nIl saldo finale \u00E8 ").concat(_this.balanceInit + primoVersamento + secondoVersamento - primoPrelievo - secondoPrelievo, "\u20AC");
-        };
         _this.addInterest = function (primoVersamento, primoPrelievo, secondoVersamento, secondoPrelievo) {
             return "".concat(_this.twoWithDraw(primoVersamento, primoPrelievo, secondoVersamento, secondoPrelievo), "\nL'interesse maturato \u00E8 di ").concat((_this.balanceInit + primoVersamento + secondoVersamento - primoPrelievo - secondoPrelievo) / 100 * 20, "\u20AC\nIl saldo netto \u00E8 ").concat((_this.balanceInit + primoVersamento + secondoVersamento - primoPrelievo - secondoPrelievo) - (_this.balanceInit + primoVersamento + secondoVersamento - primoPrelievo - secondoPrelievo) / 100 * 20, "\u20AC");
         };
@@ -59,5 +47,7 @@ var MotherAccount = /** @class */ (function (_super) {
 }(SonAccount));
 var sonAccount = new SonAccount(0);
 var momAccount = new MotherAccount(0);
-//console.log(sonAccount.twoWithDraw(1000, 10,750,45))
+console.log('SonAccount');
+console.log(sonAccount.twoWithDraw(1000, 10, 750, 45));
+console.log('MotherAccount');
 console.log(momAccount.addInterest(300, 34, 890, 564));
--- Settimana09/Giorno02/Esercitazione/assets/js/esercizio.ts
+++ Settimana09/Giorno02/Esercitazione/assets/js/esercizio.ts
@@ -6,19 +6,19 @@ class SonAccount {
         
     }
     oneDeposit=(primoVersamento:number):string=>{
-       return `Saldo Iniziale = ${this.balanceInit}€\nPrimo Versamento = ${primoVersamento}€`
+       return `Saldo Iniziale ${this.balanceInit}€\nPrimo Versamento ${primoVersamento}€`
     }
 
     oneWhiteDraw = (primoVersamento:number, primoPrelievo:number ):string =>{
-       return `${this.oneDeposit(primoVersamento)}\nPrimo Prelievo = ${primoPrelievo}€`
+       return `${this.oneDeposit(primoVersamento)}\nPrimo Prelievo ${primoPrelievo}€`
     }
 
     twoDeposit = (primoVersamento:number, primoPrelievo:number, secondoVersamento:number):string =>{
-        return `${this.oneWhiteDraw(primoVersamento,primoPrelievo)}\nSecondo Versamento = ${secondoVersamento}€`
+        return `${this.oneWhiteDraw(primoVersamento,primoPrelievo)}\nSecondo Versamento ${secondoVersamento}€`
     }
 
     twoWithDraw = (primoVersamento:number, primoPrelievo:number, secondoVersamento:number, secondoPrelievo:number):string=>{
-        return `${this.twoDeposit(primoVersamento,primoPrelievo,secondoVersamento)}\nSecondo Prelievo = ${secondoPrelievo}€\nIl saldo finale è = ${this.balanceInit+primoVersamento+secondoVersamento-primoPrelievo-secondoPrelievo}€`
+        return `${this.twoDeposit(primoVersamento,primoPrelievo,secondoVersamento)}\nSecondo Prelievo ${secondoPrelievo}€\nIl saldo finale è = ${this.balanceInit+primoVersamento+secondoVersamento-primoPrelievo-secondoPrelievo}€`
     }
 }
 
@@ -28,21 +28,7 @@ class MotherAccount extends SonAccount{
     constructor(_balanceInit:number){
         super(_balanceInit)        
     }
-    oneDeposit=(primoVersamento:number):string=>{
-        return `Saldo Iniziale = ${this.balanceInit}€\nPrimo Versamento ${primoVersamento}€`
-     }
- 
-     oneWhiteDraw = (primoVersamento:number, primoPrelievo:number ):string =>{
-        return `${this.oneDeposit(primoVersamento)}\nPrimo Prelievo ${primoPrelievo}€`
-     }
- 
-     twoDeposit = (primoVersamento:number, primoPrelievo:number, secondoVersamento:number):string =>{
-         return `${this.oneWhiteDraw(primoVersamento,primoPrelievo)}\nSecondo Versamento ${secondoVersamento}€`
-     }
- 
-     twoWithDraw = (primoVersamento:number, primoPrelievo:number, secondoVersamento:number, secondoPrelievo:number):string=>{
-         return `${this.twoDeposit(primoVersamento,primoPrelievo,secondoVersamento)}\nSecondo Prelievo ${secondoPrelievo}€\nIl saldo finale è ${this.balanceInit+primoVersamento+secondoVersamento-primoPrelievo-secondoPrelievo}€`
-     }
+
      addInterest =(primoVersamento:number, primoPrelievo:number, secondoVersamento:number, secondoPrelievo:number):string =>{
         return `${this.twoWithDraw(primoVersamento,primoPrelievo,secondoVersamento,secondoPrelievo)}\nL'interesse maturato è di ${(this.balanceInit+primoVersamento+secondoVersamento-primoPrelievo-secondoPrelievo)/100*20}€\nIl saldo netto è ${(this.balanceInit+primoVersamento+secondoVersamento-primoPrelievo-secondoPrelievo)-(this.balanceInit+primoVersamento+secondoVersamento-primoPrelievo-secondoPrelievo)/100*20}€`
      }
@@ -52,5 +38,7 @@ class MotherAccount extends SonAccount{
 let sonAccount = new SonAccount(0)
 let momAccount = new MotherAccount(0)
 
-//console.log(sonAccount.twoWithDraw(1000, 10,750,45))
+console.log('SonAccount')
+console.log(sonAccount.twoWithDraw(1000, 10,750,45))
+console.log('MotherAccount')
 console.log(momAccount.addInterest(300,34,890,564))
