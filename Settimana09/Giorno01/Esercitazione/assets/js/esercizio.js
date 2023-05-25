add9def411bf95262756189befcf65fb330dea79
SanteCalderisi112358 <santecalderisi@gmail.com>
2023-05-25 07:08:38 +0200

    er

--- Settimana09/Giorno01/Esercitazione/assets/js/esercizio.js
+++ Settimana09/Giorno01/Esercitazione/assets/js/esercizio.js
@@ -2,15 +2,15 @@ function esercizio() {
     let random_number = Math.floor(Math.random() * (100 - 1) + 1);
     document.getElementById('random_num').innerText = String(random_number);
     if (Number(document.getElementById('giocatore01').value) === random_number) {
-        document.getElementById('result').innerHTML = `Il Giocatore 1 ha indovinato!`;
+        document.getElementById('result').innerText = `Il Giocatore 1 ha indovinato!`;
     }
     else if (Number(document.getElementById('giocatore02').value) === random_number) {
-        document.getElementById('result').innerHTML = `Il Giocatore 2 ha indovinato!`;
+        document.getElementById('result').innerText = `Il Giocatore 2 ha indovinato!`;
     }
     else if (Math.abs(Number(document.getElementById('giocatore01').value) - random_number) < Math.abs(Number(document.getElementById('giocatore02').value) - random_number)) {
-        document.getElementById('result').innerHTML = `Nessuno dei due ha azzeccato il numero casuale ${random_number}, ma il Giocatore 1 si è avvicinato!`;
+        document.getElementById('result').innerText = `Nessuno dei due ha azzeccato il numero casuale ${random_number}, ma il Giocatore 1 si è avvicinato!`;
     }
     else {
-        document.getElementById('result').innerHTML = `Nessuno dei due ha azzeccato il numero casuale ${random_number}, ma il Giocatore 2 si è avvicinato!`;
+        document.getElementById('result').innerText = `Nessuno dei due ha azzeccato il numero casuale ${random_number}, ma il Giocatore 2 si è avvicinato!`;
     }
 }
--- Settimana09/Giorno01/Esercitazione/assets/js/esercizio.ts
+++ Settimana09/Giorno01/Esercitazione/assets/js/esercizio.ts
@@ -4,14 +4,14 @@ function esercizio(){
     
     let random_number:number = Math.floor(Math.random()*(100-1)+1);
     (document.getElementById('random_num') as HTMLElement).innerText = String(random_number)
-    if(Number((document.getElementById('giocatore01') as HTMLInputElement).value)===random_number){
-        (document.getElementById('result') as HTMLElement).innerHTML = `Il Giocatore 1 ha indovinato!`
-    }else if(Number((document.getElementById('giocatore02') as HTMLInputElement).value) ===random_number){
-        (document.getElementById('result') as HTMLElement).innerHTML = `Il Giocatore 2 ha indovinato!`
+    if(Number((document.getElementById('giocatore01') as HTMLInputElement).value) === random_number){
+        (document.getElementById('result') as HTMLElement).innerText = `Il Giocatore 1 ha indovinato!`
+    }else if(Number((document.getElementById('giocatore02') as HTMLInputElement).value) === random_number){
+        (document.getElementById('result') as HTMLElement).innerText = `Il Giocatore 2 ha indovinato!`
     }else if(Math.abs(Number((document.getElementById('giocatore01') as HTMLInputElement).value)-random_number)<Math.abs(Number((document.getElementById('giocatore02') as HTMLInputElement).value)-random_number)){
-        (document.getElementById('result') as HTMLElement).innerHTML = `Nessuno dei due ha azzeccato il numero casuale ${random_number}, ma il Giocatore 1 si è avvicinato!`
+        (document.getElementById('result') as HTMLElement).innerText = `Nessuno dei due ha azzeccato il numero casuale ${random_number}, ma il Giocatore 1 si è avvicinato!`
     }else{
-        (document.getElementById('result') as HTMLElement).innerHTML = `Nessuno dei due ha azzeccato il numero casuale ${random_number}, ma il Giocatore 2 si è avvicinato!`
+        (document.getElementById('result') as HTMLElement).innerText = `Nessuno dei due ha azzeccato il numero casuale ${random_number}, ma il Giocatore 2 si è avvicinato!`
     }
     
    
